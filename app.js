cd90e796db6c34234358e88d32fa17d65dd83c54
s4msecurity <81925511+s4msecurity@users.noreply.github.com>
2021-07-07 14:43:54 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const scan = require("./functionsDir/nScanner");
+const term = require("./functionsDir/command");
 const readline = require('readline-sync');
 
+
 var urlAdress, scanType;
+
 
 console.log(`
 
@@ -17,45 +20,67 @@ console.log(`
 Basic network scanner tools :) 
 `);
 
+
 function nodeMapStart() {
     urlAdress = readline.question(`Please input for url address or IP address[example: 192.168.0.1]: `);
     scanType = readline.question(`
 __________________________________________________________________________________________________
-    Scanner Type: 
-    T  = Basic              | Scans supplied hosts without portscan(-sP). Use for a quick discovery.
-    O  = OS                 | Scans for open ports as well as NMAP gathered OS information.
-    Q  = Queded             | Queued version for greater control
-    S  = NScanner           | Special Scannaer, manuel. Example input: [-sP -sV -A ]
-    UN = Network USer List  | User found in network
-    [example: T]: `)
-    checkedType(scanType)
+|    Scanner Type: 
+|    T  = Basic                      | Scans supplied hosts without portscan(-sP). Use for a quick discovery.
+|    O  = OS                         | Scans for open ports as well as NMAP gathered OS information.
+|    Q  = Queded                     | Queued version for greater control
+|    S  = NScanner                   | Special Scannaer, manuel. Example input: [-sP -sV -A ]
+|    UN = Network Local user list    | User found in network
+|    [example: T]: `)
+    checkedStartType(scanType)
 }
 
-function checkedType(inp) {
-    if ((inp == null || scanType == undefined) || (urlAdress == null || urlAdress == undefined)) {
 
-        console.log("PlePlease do not enter null value restarting. :| ", nodeMapStart());
+function cmdSet() {
+    scanType = readline.question(`
+__________________________________________________________________
+|    Scanner Type: 
+|    UL  = User list          | View users in the public network
+|    AL  = Arp list           | View the ARP Table.
+|    PL  = Ping Send          | Send ping
+|    [example: UL]: `)
+    inpCmd(scanType);
+}
 
-    } 
+
+function inpCmd(inp) {
+    if (inp == "PL") {
+        if (urlAdress == null || urlAdress == undefined || urlAdress == "") {
+            urlAdress = readline.question(`Please input for url address or IP address[example: 192.168.0.1]: `);
+            term.nCommand(inp, urlAdress);
+        } else {
+            term.nCommand(inp, urlAdress);
+        }
+    }
     else {
-        if (inp == "S") {
-
-            sParameters = readline.question("   \nEnter the special parameters to scan [Example: input like '-sP -A -sV...']: ");
-            
-            scan.momNFunction(urlAdress, inp, sParameters)
-
-        }
-        else if (inp != "N") {
-
-            scan.momNFunction(urlAdress, inp, "");
-
-        }
-        else {
-
-            console.log("Programming exit.");
-
-        }
+        term.nCommand(inp, "")
     }
 }
 
+
+function checkedStartType(inp) {
+    if ((inp == null || inp == undefined || inp == "")) {
+        console.log("PlePlease do not enter null value restarting. :| ", nodeMapStart());
+    }
+    else {
+        if (inp == "S") {
+            sParameters = readline.question("   \nEnter the special parameters to scan [Example: input like '-sP -A -sV...']: ");
+            scan.momNFunction(urlAdress, inp, sParameters)
+        }
+        else if (inp != "UN") {
+            scan.momNFunction(urlAdress, inp, "");
+        }
+        else if (inp == "UN") {
+            cmdSet();
+        }
+        else {
+            console.log("Programming exit.");
+        }
+    }
+}
 nodeMapStart();
--- functionsDir/nScanner.js
+++ functionsDir/nScanner.js
@@ -3,38 +3,38 @@ const nmap = require("node-nmap")
 nmap.nmapLocation = "nmap"
 
 
-function QuickScript(urlAdress, scanType){
+function QuickScript(urlAdress, scanType) {
 
-    let quickscan = new nmap.QuickScan("127.0.0.1 "+urlAdress)
-    quickscan.on("complete", function(data){
+    let quickscan = new nmap.QuickScan("127.0.0.1 " + urlAdress)
+    quickscan.on("complete", function (data) {
         console.log(data)
     })
-    quickscan.on("error", function(error){
+    quickscan.on("error", function (error) {
         console.log(error)
     })
     quickscan.startScan()
 }
 
-function OsAndPortScript(urlAdress, scanType){
+function OsAndPortScript(urlAdress, scanType) {
 
-    let quickscan = new nmap.OsAndPortScan("127.0.0.1 "+urlAdress)
-    quickscan.on("complete", function(data){
+    let quickscan = new nmap.OsAndPortScan("127.0.0.1 " + urlAdress)
+    quickscan.on("complete", function (data) {
         console.log(data)
     })
-    quickscan.on("error", function(error){
+    quickscan.on("error", function (error) {
         console.log(error)
     })
     quickscan.startScan()
 }
 
 
-function specialcanner(urlAdress, scanType, parameter){
+function specialcanner(urlAdress, scanType, parameter) {
 
-    let quickscan = new nmap.NmapScan("127.0.0.1 "+urlAdress, parameter)
-    quickscan.on("complete", function(data){
+    let quickscan = new nmap.NmapScan("127.0.0.1 " + urlAdress, parameter)
+    quickscan.on("complete", function (data) {
         console.log(data)
     })
-    quickscan.on("error", function(error){
+    quickscan.on("error", function (error) {
         console.log(error)
     })
     quickscan.startScan()
@@ -42,16 +42,16 @@ function specialcanner(urlAdress, scanType, parameter){
 }
 
 
-module.exports.momNFunction = function(urlAdress, scanType, parameter){
+module.exports.momNFunction = function (urlAdress, scanType, parameter) {
 
-    switch(scanType){
+    switch (scanType) {
         case "T":
             QuickScript(urlAdress, scanType);
         case "O":
             OsAndPortScript(urlAdress, scanType);
         case "S":
             specialcanner(urlAdress, scanType, parameter);
-        break;            
-    }    
+            break;
+    }
 
 }
